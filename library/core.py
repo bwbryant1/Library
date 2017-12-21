2a8efe7f70129fc74ebd20f53c28216826c29f5b
Brandon <brandon@Brandons-Macbook.local>
2017-12-21 14:43:17 -0600

    added PyQt5 requirement and added a test

--- library/core.py
+++ library/core.py
@@ -2,12 +2,18 @@
 # coding: utf-8
 
 import sys
-
 from library import libraryItem as item
+from PyQt5 import QtWidgets
 
 def main():
-	libObj = item.libraryItem("45","Test Name","Test Desc")
-	print(libObj)
+    libObj = item.libraryItem("45","Test Name","Test Desc")
+    print(libObj)
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+    button = QtWidgets.QPushButton("Begin PyQt5 window test")
+    window.setCentralWidget(button)
+    window.show()
+    app.exec_()
 
 # wow very main
 if __name__ == "__main__":
